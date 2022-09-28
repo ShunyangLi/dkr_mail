638b666de7336590a313451370b153183859e695
Shunyang Li <shunyang.li@unsw.edu.au>
2022-09-28 15:47:03 +1000

    add the counting function

--- backend/api/mail_handling.py
+++ backend/api/mail_handling.py
@@ -60,8 +60,8 @@ class History(Resource):
     @mails.response(200, 'Success')
     @mails.response(400, 'Missing args')
     @mails.doc(description="Three presenters and their present date")
-    def post(self):
-        data = query_db("select name, count(name) from past group by name")
+    def get(self):
+        data = query_db("select name, count(name) as number from past group by name")
 
         return make_response(jsonify({"message": "success", "data": data}), 200)
 
--- frontend/src/views/Result/Result.jsx
+++ frontend/src/views/Result/Result.jsx
@@ -1,89 +1,89 @@
 import React, { Component } from "react";
-import { Column } from '@ant-design/plots';
+import { Column } from "@ant-design/plots";
 import CustomBreadcrumb from "@/components/CustomBreadcrumb/CustomBreadcrumb";
 import "./index.scss";
+import { message } from "antd";
+import axios from "@/api";
+import { API } from "@/api/config";
 
-const data = [
-  {
-    type: '家具家电',
-    sales: 38,
-  },
-  {
-    type: '粮油副食',
-    sales: 52,
-  },
-  {
-    type: '生鲜水果',
-    sales: 61,
-  },
-  {
-    type: '美容洗护',
-    sales: 145,
-  },
-  {
-    type: '母婴用品',
-    sales: 48,
-  },
-  {
-    type: '进口食品',
-    sales: 38,
-  },
-  {
-    type: '食品饮料',
-    sales: 38,
-  },
-  {
-    type: '家庭清洁',
-    sales: 38,
-  },
-];
-const config = {
-  data,
-  xField: 'type',
-  yField: 'sales',
-  label: {
-    // 可手动配置 label 数据标签位置
-    position: 'middle',
-    // 'top', 'bottom', 'middle',
-    // 配置样式
-    style: {
-      fill: '#FFFFFF',
-      opacity: 0.6,
-    },
-  },
-  xAxis: {
-    label: {
-      autoHide: true,
-      autoRotate: false,
-    },
-  },
-  meta: {
-    type: {
-      alias: '类别',
-    },
-    sales: {
-      alias: '销售额',
-    },
-  },
-};
 class Result extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      search: "csv",
-      loading: false,
-      fileList: []
+      config: {
+        data: [],
+        xField: "name",
+        yField: "number",
+        label: {
+          // 可手动配置 label 数据标签位置
+          position: "middle",
+          // 'top', 'bottom', 'middle',
+          // 配置样式
+          style: {
+            fill: "#FFFFFF",
+            opacity: 0.6
+          }
+        },
+        xAxis: {
+          label: {
+            autoHide: false,
+            autoRotate: true
+          }
+        },
+        meta: {
+          name: {
+            alias: "name"
+          },
+          number: {
+            alias: "number"
+          }
+        }
+      }
     };
   }
 
+  componentDidMount() {
+    axios
+      .get(`${API}/user/history`, {})
+      .then(res => {
+        let temp = [];
+        res.data.data.forEach(c => {
+          temp.push({
+            name: c.name,
+            number: c.number
+          });
+        });
+
+        let config = { ...this.state.config };
+        config.data = temp;
+        this.setState({
+          config: config
+        });
+
+        console.log(this.state.config);
+      })
+      .catch(function(error) {
+        message.error(error.message);
+      });
+  }
 
   render() {
-
+    // const {config} = this.state;
     return (
       <div>
         <CustomBreadcrumb arr={["result"]} />
-        <div style={{ width: "90%", marginTop: "5%", margin: "auto", top: 2, bottom: 0, left: 0, right: 0}}>
-          <Column {...config} />
+        <div
+          style={{
+            width: "90%",
+            marginTop: "5%",
+            margin: "auto",
+            top: 2,
+            bottom: 0,
+            left: 0,
+            right: 0
+          }}
+        >
+          <Column {...this.state.config} style={{ marginTop: "5%" }} />
         </div>
       </div>
     );
